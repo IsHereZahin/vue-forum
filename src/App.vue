1ac9f6cde81b433420c58fc71ca7c7516162d97b
Abu Zahin Mohammad Nowsin <abuzahinmohammadnowsin@gmail.com>
2024-04-17 13:01:17 +0600

    Display list of elements in Vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PageHome/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import PageHome from './components/PageHome.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    PageHome,
   }
 }
 </script>
---
+++ src/components/PageHome.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <!-- Your component content here -->
+    <h1>Welcome to Home Page</h1>
+    <p>This is the home page of your Vue forum.</p>
+    <div v-for="thread in threads" :key="thread.id">
+      <h2>{{ thread.title }}</h2>
+      <div v-for="postId in thread.posts" :key="postId">
+
+        <!-- <p>{{ users.find(u => u.id === posts.find(p => p.id === postId).userId).name }}</p>
+        <p>{{ posts.find(p => p.id === postId).text }}</p> -->
+
+        <!-- If not using the methods, use it -->
+        <p>{{ getUserByPostId(postId).name }}</p>
+        <p>{{ getPostTextById(postId).text }}</p>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '../data.json';
+
+export default {
+  // Component options here
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users,
+    };
+  },
+
+  methods: {
+    getUserByPostId(postId) {
+      const userId = this.posts.find(post => post.id === postId).userId;
+      return this.users.find(user => user.id === userId);
+    },
+    getPostTextById(postId) {
+      return this.posts.find(post => post.id === postId);
+    }
+  }
+
+};
+</script>
+
+<style scoped>
+/* Component styles here */
+</style>
