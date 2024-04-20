cadadafe2d98b3f02155d076ba62fa8771bdcaeb
Abu Zahin Mohammad Nowsin <abuzahinmohammadnowsin@gmail.com>
2024-04-20 15:23:59 +0600

    design improve

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,19 +1,14 @@
 <template>
-  <div>
-    <!-- Your component content here -->
-    <h1>Welcome to Home Page</h1>
-    <p>This is the home page of your Vue forum.</p>
-    <div v-for="thread in threads" :key="thread.id">
+  <div class="home">
+    <h1>Welcome to Our Forum</h1>
+    <div v-for="thread in threads" :key="thread.id" class="thread">
       <h2>{{ thread.title }}</h2>
-      <div v-for="postId in thread.posts" :key="postId">
-
-        <!-- <p>{{ users.find(u => u.id === posts.find(p => p.id === postId).userId).name }}</p>
-        <p>{{ posts.find(p => p.id === postId).text }}</p> -->
-
-        <!-- If not using the functions, use it -->
-        <p>{{ getUserByPostId(postId).name }}</p>
-        <p>{{ getPostTextById(postId).text }}</p>
-
+      <div v-for="postId in thread.posts" :key="postId" class="post">
+        <div class="user-info">
+          <img src="../assets/avatar.png" alt="User Avatar" class="avatar" />
+          <p class="username">{{ getUserByPostId(postId).name }}</p>
+        </div>
+        <p class="post-text">{{ getPostTextById(postId).text }}</p>
       </div>
     </div>
   </div>
@@ -21,29 +16,65 @@
 
 <script setup>
 import sourceData from '../data.json';
-// import { reactive } from 'vue';
-// const threads = reactive(sourceData.threads)
-// const posts   = reactive(sourceData.posts)
-// const users   = reactive(sourceData.users)
-
 import { ref } from 'vue';
 
 const threads = ref(sourceData.threads)
-const posts   = ref(sourceData.posts)
-const users   = ref(sourceData.users)
+const posts = ref(sourceData.posts)
+const users = ref(sourceData.users)
 
-function getUserByPostId(postId)
-{
-  const userId = this.posts.find(post => post.id === postId).userId;
-  return users.value.find(user => user.id === userId);
+function getUserByPostId(postId) {
+  const post = posts.value.find(post => post.id === postId);
+  const user = users.value.find(user => user.id === post.userId);
+  return user;
 }
-function getPostTextById(postId)
-{
+
+function getPostTextById(postId) {
   return posts.value.find(post => post.id === postId);
 }
-
 </script>
 
 <style scoped>
-/* Component styles here */
+.home {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+
+  .thread {
+    margin-bottom: 30px;
+    border-bottom: 1px solid #ccc;
+    padding-bottom: 20px;
+  }
+
+  .thread h2 {
+    color: #333;
+    font-size: 24px;
+  }
+
+  .post {
+    display: flex;
+    align-items: flex-start;
+    margin-top: 15px;
+  }
+
+  .user-info {
+    margin-right: 15px;
+  }
+
+  .avatar {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+
+  .username {
+    font-weight: bold;
+    margin-top: 5px;
+  }
+
+  .post-text {
+    font-size: 16px;
+    line-height: 1.5;
+    color: #555;
+  }
 </style>
